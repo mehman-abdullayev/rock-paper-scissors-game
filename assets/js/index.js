e473c32b44f66f8f9618cf938c5bb2089b599656
mehman-abdullayev <122769508+mehman-abdullayev@users.noreply.github.com>
2023-03-05 13:13:06 +0400

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+import { getDatabase, get, set, ref, push, remove, onValue } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBIENxgZnjHSBUt9urGtECDocGtK2FxfVo",
+    authDomain: "iatc-task.firebaseapp.com",
+    databaseURL: "https://iatc-task-default-rtdb.firebaseio.com",
+    projectId: "iatc-task",
+    storageBucket: "iatc-task.appspot.com",
+    messagingSenderId: "261539857862",
+    appId: "1:261539857862:web:58a83294a98df4914c9ec7"
+};
+
+const app = initializeApp(firebaseConfig), db = getDatabase(app);
+
+document.getElementById('form').addEventListener('submit', addPlayer());
+
+function addPlayer(e) {
+	e.preventDefault();
+	set(ref(db, '/' + 'user'), {
+		userName: 'mehman',
+		phone: '0508619691'
+	});
+}
